f24a4a7ec0cd79db46bbf0f36e81320749bf603f
TUCOSALAMANKA <sparowj765@gmail.com>
2024-10-13 14:50:37 +0300

    Input flags

--- main.cpp
+++ main.cpp
@@ -3,5 +3,21 @@ int main()
 {
   int a = 0;
   std::cin >>  a;
+  if (std::cin.good())
+  {
+    std::cerr << "Good" << '\n';	
+  }
+  else if (std::cin.eof())
+  {
+    std::cerr << "Eof" << '\n';
+  }
+  else if (std::cin.bad())
+  {
+    std::cerr << "Bad" << '\n';
+  }
+  else if (std::cin.fail())
+  {
+    std::cerr << "Fail" << '\n';
+  }
   std::cout << a / 2 << '\n';
 }
